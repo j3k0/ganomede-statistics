1cdc283e9c18ba57835feb637c9cf317595ec30c
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-08-06 11:35:12 +0300

    Update config for statistics

--- config.js
+++ config.js
@@ -4,21 +4,14 @@ module.exports = {
   port: +process.env.PORT || 8000,
   routePrefix: process.env.ROUTE_PREFIX || pkg.api,
 
-  rules: {
-    host: process.env.RULES_PORT_8080_TCP_ADDR || 'localhost',
-    port: +process.env.RULES_PORT_8080_TCP_PORT || 8080
-  },
-
-  authdb: {
-    host: process.env.REDIS_AUTH_PORT_6379_TCP_ADDR || 'localhost',
-    port: +process.env.REDIS_AUTH_PORT_6379_TCP_PORT || 6379
+  coordinator: {
+    host: process.env.COORDINATOR_PORT_8080_TCP_ADDR || 'localhost',
+    port: +process.env.COORDINATOR_PORT_8080_TCP_PORT || 8080
   },
 
   redis: {
-    host: process.env.REDIS_GAMES_PORT_6379_TCP_ADDR || 'localhost',
-    port: +process.env.REDIS_GAMES_PORT_6379_TCP_PORT || 6379,
+    host: process.env.REDIS_STATISTICS_PORT_6379_TCP_ADDR || 'localhost',
+    port: +process.env.REDIS_STATISTICS_PORT_6379_TCP_PORT || 6379,
     prefix: pkg.api
   }
-// COUCH_GAMES_PORT_5984_TCP_ADDR - IP of the games couchdb
-// COUCH_GAMES_PORT_5984_TCP_PORT
 };
