ede83d3b5fcaa63c2d557607efde0bda25ed260e
Jean-Christophe Hoelt <hoelt@fovea.cc>
2018-06-21 07:35:47 +0300

    Allow user to define MAX_OLD_SPACE_SIZE

--- statistics-worker.sh
+++ statistics-worker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "`dirname $0`"
 while true; do
-    (./node_modules/.bin/coffee statistics-worker.coffee || sleep 4)
+    (
+      ./node_modules/.bin/coffee \
+        --nodejs --max-old-space-size=${MAX_OLD_SPACE_SIZE:-1024} \
+        statistics-worker.coffee \
+      || sleep 4
+    )
     sleep 1
 done
